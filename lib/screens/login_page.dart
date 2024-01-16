af53fb44a567a0abba93945f1851c88810f1f1e6
bibin k b <bibinkochumalayil007@gmail.com>
2024-01-16 23:21:38 +0530

    login ui completed

---
+++ lib/screens/login_page.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool isVisible = true;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          height: double.infinity,
+          width: double.infinity,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 18.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Login',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                      label: Text('Username'),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15))),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "enter a valid username";
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  obscureText: isVisible,
+                  decoration: InputDecoration(
+                      suffixIcon: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            isVisible = !isVisible;
+                          });
+                        },
+                        icon: isVisible == true
+                            ? Icon(Icons.visibility_off)
+                            : Icon(Icons.visibility),
+                      ),
+                      label: Text('Password'),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15))),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "enter a valid Password";
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextButton(
+                    onPressed: () {
+                      // Logic for Login
+                    },
+                    child: Text("Login"))
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
