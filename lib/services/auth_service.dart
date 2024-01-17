f4d4fa2a710012342701760c4804aebbe8921dfb
bibin k b <bibinkochumalayil007@gmail.com>
2024-01-17 23:42:56 +0530

    Auth service completed

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,20 +1,35 @@
+import 'dart:convert';
+
 import 'package:api/api/api_key.dart';
 import 'package:api/model/user_model.dart';
-import 'package:http/http.dart' as http ;
-
+import 'package:http/http.dart' as http;
 
 class AuthService {
   final client = http.Client();
 
+  Future<UserModel?> login(String username, String password) async {
+    final Map<String, dynamic> body = {
+      'username': username,
+      'password': password,
+    };
+    try {
+      final response =
+          await client.post(Uri.parse("${API().baseUrl}${API().authUrl}"),
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: jsonEncode(body));
 
- Future <UserModel> login(String username , String password) async{
-
-try {
-  final response = await client.post(
-    Uri.parse("${API().baseUrl}${API().authUrl}"),
-  );
-}
- }
-
-
+      if (response.statusCode == 200) {
+        final result = jsonDecode(response.body);
+        final UserModel user = UserModel.fromJson(result);
+        return user;
+      } else {
+        print("Request failed with status code ${response.statusCode}");
+      }
+    } catch (error) {
+      print('error');
+    }
+    return null;
+  }
 }
